8fc231eacd1bab65b0cdb944a6289763a2168a42
Christopher Hall <mythmakr@live.com>
2012-03-28 14:17:41 -0700

    PMB Chrome extension skeleton

---
+++ pimpmybasecamp/bootstrap.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('head').append("<script type='text/javascript' src='http://dl.dropbox.com/u/15187398/pmb/js/remote.js'></script>");
+	$('head').append("<link rel='stylesheet' type='text/css' src='http://dl.dropbox.com/u/15187398/pmb/css/remote.css' />");
+});
+
+// css: http://dl.dropbox.com/u/15187398/pmb/css/remote.css
+// js: http://dl.dropbox.com/u/15187398/js/remote.js
