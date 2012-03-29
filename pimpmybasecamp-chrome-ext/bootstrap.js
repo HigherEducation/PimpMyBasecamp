2cb963898edf0bb18d796c71acbd493b462be90a
Christopher Hall <mythmakr@live.com>
2012-03-28 17:20:04 -0700

    Provides a mechanism for using remote css using xhr
    Refs #1

--- pimpmybasecamp-chrome-ext/bootstrap.js
+++ pimpmybasecamp-chrome-ext/bootstrap.js
@@ -1,2 +1,10 @@
 $('head').append('<script type="text/javascript" src="https://raw.github.com/CMN/PimpMyBasecamp/develop/pimpmybasecamp-chrome-ext/recipes/all.js"></script>');
-$('head').append('<link rel="stylesheet" type="text/css" src="https://raw.github.com/CMN/PimpMyBasecamp/develop/pimpmybasecamp-chrome-ext/recipes/all.css" />');
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://raw.github.com/CMN/PimpMyBasecamp/develop/pimpmybasecamp-chrome-ext/recipes/all.css", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    $('head').append('<style type="text/css">' + xhr.responseText + '</style>');
+  }
+}
+xhr.send();
