2d4b5993bbb2fda9e60844224e9b996a8e5701a4
Christopher Hall <mythmakr@live.com>
2012-03-28 17:13:58 -0700

    Update links in bootstrap.js to reflect folder name change

--- pimpmybasecamp-chrome-ext/bootstrap.js
+++ pimpmybasecamp-chrome-ext/bootstrap.js
@@ -1,2 +1,2 @@
-$('head').append('<script type="text/javascript" src="https://raw.github.com/CMN/PimpMyBasecamp/develop/pimpmybasecamp/recipes/all.js"></script>');
-$('head').append('<link rel="stylesheet" type="text/css" src="https://raw.github.com/CMN/PimpMyBasecamp/develop/pimpmybasecamp/recipes/all.css" />');
+$('head').append('<script type="text/javascript" src="https://raw.github.com/CMN/PimpMyBasecamp/develop/pimpmybasecamp-chrome-ext/recipes/all.js"></script>');
+$('head').append('<link rel="stylesheet" type="text/css" src="https://raw.github.com/CMN/PimpMyBasecamp/develop/pimpmybasecamp-chrome-ext/recipes/all.css" />');
